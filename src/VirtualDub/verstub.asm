b87550af0f1a81d120b4820892b1dc73a5d185aa
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2020-01-17 14:17:09 +0100

    Initial commit

---
+++ src/VirtualDub/verstub.asm
@@ -0,0 +1,13 @@
+	segment	.const
+
+	global	_version_num
+	global	_version_time
+	global	_version_date
+	global	_version_buildmachine
+
+_version_num	dd	35495
+_version_time	db	"Fri Jan 17 14:02:17 2020",0
+_version_date	db	"January 17, 2020",0
+_version_buildmachine	db	"WS-4770",0
+
+	end
