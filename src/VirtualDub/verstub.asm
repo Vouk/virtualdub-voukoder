9e5656b3d5f035ec187a2e1205135a8cde60c7c5
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2020-01-20 09:57:19 +0100

    Fixed build config

--- src/VirtualDub/verstub.asm
+++ src/VirtualDub/verstub.asm
@@ -5,8 +5,8 @@
 	global	version_date
 	global	version_buildmachine
 
-version_num	dd	35623
-version_time	db	"Mon Jan 20 09:33:59 2020",0
+version_num	dd	35624
+version_time	db	"Mon Jan 20 09:56:58 2020",0
 version_date	db	"January 20, 2020",0
 version_buildmachine	db	"WS-4770",0
 
